83828bc4c3ee0f37413007a6ec1c88edeaea87e0
sandraviswanath <sandraviswanath2@gmail.com>
2024-07-27 17:09:44 +0530

    responsive-animation

--- my-app/src/Components/Animation/AirplaneWindow.jsx
+++ my-app/src/Components/Animation/AirplaneWindow.jsx
@@ -1,9 +1,12 @@
+
+
+
 import React, { useEffect, useState } from 'react';
 import './AirplaneWindow.css';
 import img1 from '../../images/img1.png';
 import img2 from '../../images/img2.png';
 import img3 from '../../images/img3.png';
-import { Col, Row } from 'react-bootstrap'
+
 const AirplaneWindow = () => {
     const [windowState, setWindowState] = useState('closed');
 
@@ -13,13 +16,9 @@ const AirplaneWindow = () => {
 
         const interval = setInterval(() => {
             setWindowState('open');
-          
-            setTimeout(() => setWindowState('open'), step);
             setTimeout(() => setWindowState('idle'), step * 2);
             setTimeout(() => setWindowState('closed'), step * 3);
-            setTimeout(() => setWindowState('closed'), step * 4);
             setTimeout(() => setWindowState('idle'), step * 5);
-            setTimeout(() => setWindowState('open'), step * 6);
         }, animationInterval);
 
         return () => clearInterval(interval);
@@ -27,24 +26,19 @@ const AirplaneWindow = () => {
 
     return (
         <div className="containers">
-            {/* <div className="background"></div> */}
-            <div className={`window1 ${windowState}`}>
-                <img src={img1} className="image1" alt="Window 1" />
+            <div className={`window ${windowState}`}>
+                <img src={img1} className="image" alt="Window 1" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
-            <div className={`window2 ${windowState}`}>
-                <img src={img2} className="image2" alt="Window 2" />
+            <div className={`window ${windowState}`}>
+                <img src={img2} className="image" alt="Window 2" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
             <div className={`window ${windowState}`}>
                 <img src={img3} className="image" alt="Window 3" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
-
         </div>
-
-        
-      
     );
 };
 
