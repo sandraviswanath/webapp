63287f25fd6c83b2c8b9804d2ec972ba4a1a1d49
sandraviswanath <sandraviswanath2@gmail.com>
2024-07-27 14:23:41 +0530

    animation-card head- responsive

--- my-app/src/Components/Animation/AirplaneWindow.jsx
+++ my-app/src/Components/Animation/AirplaneWindow.jsx
@@ -13,12 +13,7 @@ const AirplaneWindow = () => {
 
         const interval = setInterval(() => {
             setWindowState('open');
-            // setTimeout(() => setWindowState('closed'), step);
-            // setTimeout(() => setWindowState('idle'), step * 2);
-            // setTimeout(() => setWindowState('open'), step * 3);
-            // setTimeout(() => setWindowState('closed'), step * 4);
-            // setTimeout(() => setWindowState('idle'), step * 5);
-            // setTimeout(() => setWindowState('open'), step * 6);
+          
             setTimeout(() => setWindowState('open'), step);
             setTimeout(() => setWindowState('idle'), step * 2);
             setTimeout(() => setWindowState('closed'), step * 3);
@@ -45,30 +40,13 @@ const AirplaneWindow = () => {
                 <img src={img3} className="image" alt="Window 3" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
-            
+
         </div>
 
-        // <div className="containers">
-        //     <div className="background">
-        //     <Row className='container2  justify-content-md-center'>
-        //     <Col xs lg="2" className={`window1 ${windowState}`}>
-        //         <img src={img1} className="image1" alt="Window 1" />
-        //         <div className={`shutter ${windowState}`}></div>
-        //     </Col>
-        //     <Col xs lg="2" className={`window2 ${windowState}`}>
-        //         <img src={img2} className="image2" alt="Window 2" />
-        //         <div className={`shutter ${windowState}`}></div>
-        //     </Col>
-        //     <Col xs lg="2" className={`window ${windowState}`}>
-        //         <img src={img3} className="image" alt="Window 3" />
-        //         <div className={`shutter ${windowState}`}></div>
-        //     </Col>
-        //     </Row>
-        //     </div>
-        // </div>
+        
+      
     );
 };
 
 export default AirplaneWindow;
 
-// responsive
